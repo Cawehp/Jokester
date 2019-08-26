6d2af7c7491fac3f3e66d5a432c8649f6b61120e
Dennis Fransen <dennisfransen@hotmail.se>
2019-08-26 12:39:08 +0200

    Dennis contribution

--- store/actions/jokes.js
+++ store/actions/jokes.js
@@ -1,6 +1,8 @@
 export const GET_JOKE = 'GET_JOKE'
 export const ADD_JOKE= 'ADD_JOKE'
 export const REMOVE_JOKE = 'REMOVE_JOKE'
+export const SHARE_JOKE = 'SHARE_JOKE'
+
 
 export const getJoke = (joke) => {
     return {type: GET_JOKE, payload: joke}
@@ -12,4 +14,8 @@ export const addJoke = (joke) => {
 
 export const removeJoke = (joke) => {
     return {type: REMOVE_JOKE, payload: joke}
+}
+
+export const shareJoke = (joke) => {
+    return {type: SHARE_JOKE, payload: joke}
 }
--- store/reducers/jokes.js
+++ store/reducers/jokes.js
@@ -1,24 +1,21 @@
-import { ADD_JOKE, GET_JOKE, REMOVE_JOKE} from '../actions/jokes'
+import { ADD_JOKE, GET_JOKE, REMOVE_JOKE, SHARE_JOKE} from '../actions/jokes'
 
 const initialState = {
     joke: '',
-    jokeList: [{
-        id: 666,
-        key: '88888'
-    },{
-        id: 234,
-        key: 'gawad'
-    }]
+    jokeList: [],
+    sharedJokes: []
 }
 
 const jokeReducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'GET_JOKE':
+        case GET_JOKE:
             return { ...state, joke: action.payload}
-        case 'ADD_JOKE':
+        case ADD_JOKE:
             return { ...state, jokeList: state.jokeList.concat(action.payload)}
-        case 'REMOVE_JOKE':
+        case REMOVE_JOKE:
             return { ...state, jokeList: state.jokeList.filter(joke => joke.id !== action.payload)}
+        case SHARE_JOKE:
+            return { ...state, sharedJokes: state.sharedJokes.concat(action.payload) }
         default:
             break;
     }
